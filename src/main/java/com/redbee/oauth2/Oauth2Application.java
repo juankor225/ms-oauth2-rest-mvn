5529c78a8fd8e10c8c3a778e85b5e6de7a265eb8
Juan Ramirez Uriol <juanko225@gmail.com>
2019-11-25 14:53:18 -0500

    primer commit

---
+++ src/main/java/com/redbee/oauth2/Oauth2Application.java
@@ -0,0 +1,23 @@
+package com.redbee.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+@Configuration
+public class Oauth2Application extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2Application.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
---
+++ src/test/java/com/redbee/oauth2/msoauth2restmvn/MsOauth2RestMvnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.redbee.oauth2.msoauth2restmvn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsOauth2RestMvnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
